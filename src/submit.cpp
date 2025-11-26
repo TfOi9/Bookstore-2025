85eae6e5c8d8e4dde1fccdaff65a919d5478d2f6
TfOi9 <tfoi92024@sjtu.edu.cn>
2025-11-26 10:09:32 +0800

    fix: bugs in bucket update

--- src/submit.cpp
+++ src/submit.cpp
@@ -1,12 +1,67 @@
-#ifndef HASH_MAP_HPP
-#define HASH_MAP_HPP
-
 #include <iostream>
 #include <vector>
 #include <cstring>
 #include <cstdint>
 #include <algorithm>
-#include "memory_river.hpp"
+#include <fstream>
+
+using std::string;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+
+template<class T, int info_len = 2, bool auto_clear = 0>
+class MemoryRiver {
+private:
+    /* your code here */
+    fstream file;
+    string file_name;
+    int sizeofT = sizeof(T);
+    int info_offset = info_len * sizeof(int);
+public:
+    MemoryRiver() = default;
+
+    MemoryRiver(const string& file_name) : file_name(file_name) {}
+
+    ~MemoryRiver() {
+        if (file.is_open()) {
+            file.close();
+        }
+    }
+
+    bool open_file() {
+        file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+        if (!file) {
+            file.open(file_name, std::ios::out | std::ios::binary);
+            file.close();
+            file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+            return 0;
+        }
+        return 1;
+    }
+
+    bool initialise(string FN = "") {
+        if (FN != "") file_name = FN;
+        return open_file();
+    }
+
+    int write(T &t) {
+        file.seekp(0, std::ios::end);
+        int pos = file.tellp();
+        file.write(reinterpret_cast<const char*>(&t), sizeof(T));
+        return pos + 1;
+    }
+
+    void update(T &t, const int pos) {
+        file.seekp(pos - 1);
+        file.write(reinterpret_cast<char *>(&t), sizeofT);
+    }
+
+    void read(T &t, const int pos) {
+        file.seekg(pos - 1);
+        file.read(reinterpret_cast<char *>(&t), sizeofT);
+    }
+};
 
 template<typename ValueType, int BlockCapacity = 200, int HashSize = 10007>
 class HashMap {
@@ -210,5 +265,35 @@ public:
     } 
 
 };
+using namespace std;
 
-#endif
+int main() {
+    int t;
+    cin >> t;
+    HashMap<int> hshmp;
+    while (t--) {
+        string op, s;
+        int x;
+        cin >> op;
+        if (op == "insert") {
+            cin >> s >> x;
+            hshmp.insert(s.c_str(), s.size(), x);
+        }
+        else if (op == "delete") {
+            cin >> s >> x;
+            hshmp.erase(s.c_str(), s.size(), x);
+        }
+        else if (op == "find") {
+            cin >> s;
+            std::vector<int> vec = hshmp.find(s.c_str(), s.size());
+            if (!vec.size()) {
+                cout << "null" << endl;
+                continue;
+            }
+            for (int i = 0; i < vec.size(); i++) {
+                cout << vec[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
