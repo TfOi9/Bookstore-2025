941ac91b49e7f66877a30bbe3d3296d08c8b30b0
TfOi9 <tfoi92024@sjtu.edu.cn>
2025-12-12 23:38:31 +0800

    debug: added breakpoints

--- src/code.cpp
+++ src/code.cpp
@@ -209,6 +209,7 @@ int main() {
         }
         else if (op == "show") {
             if (tokens.size() > 1 && tokens[1] == "finance") {
+                if (can_kill) while(1);
                 if (account_manager.current_privilege() < 7) {
                     std::cout << "Invalid\n";
                     continue;
